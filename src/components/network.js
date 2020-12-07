b6df81874cfd20e2cb0f9eae8e0e1efe752b17ba
andresbr92 <andresbr92@gmail.com>
2020-12-07 20:46:51 +0100

    app done

--- src/components/network.js
+++ src/components/network.js
@@ -23,9 +23,10 @@ function network(props) {
   const [password, setPassword] = useState("");
 
   
-  requestMultiple([PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION, PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION]).then((statuses) => {
+  requestMultiple([PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION, PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION, PERMISSIONS.ANDROID.ACCESS_NETWORK_STATE]).then((statuses) => {
     console.log('COARSE', statuses[PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION]);
     console.log('FINE', statuses[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION]);
+    console.log('NETWORK', statuses[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION]);
   });
   // request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION ).then(res => {
   //   console.log(res)
@@ -42,7 +43,7 @@ function network(props) {
         console.log(reason)
       }))
 
- 
+ //Bus 001 Device 008: ID 04e8:6860 Samsung Electronics Co., Ltd Galaxy series, misc. (MTP mode)
 
 
 
@@ -63,7 +64,25 @@ function network(props) {
         value={password}
       />
 
-      <Button title="Crear hotspott" onPress =  {() => checkIt()}  />
+      <Button title="Crear hotspott" onPress={()=>LocalOnlyHotspot.start(
+          (data)=>{
+            console.log(data)
+           // setSsid(data)
+          },(reason)=>{
+            console.log(reason)
+            //setPassword(reason)
+          })}  />
+           {/* <Button title="obtener info" onPress={()=>LocalOnlyHotspot.getConfig(
+          (data)=>{
+            console.log(data)
+           // setSsid(data)
+          })}/>
+          <Button title="STOP" onPress={()=>LocalOnlyHotspot.stop(
+          (data)=>{
+            console.log(data)
+           // setSsid(data)
+          })}/> */}
+
     </>
   );
 }
